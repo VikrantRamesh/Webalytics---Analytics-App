4e862056cca37417cd39e7dc37cf888631ee8455
Rohit <rohitd.arsly@gmail.com>
2022-10-01 11:30:20 +0530

    added query string support

--- App/client/src/hooks/useApiCall.js
+++ App/client/src/hooks/useApiCall.js
@@ -1,15 +1,35 @@
 
+const createURL = (config) => {
+    console.log(config);
+    let url = `http://localhost:4573/api/${config.endpoint}`;
+    // for(let x of Object.entries(config.params)){
+    //     url.concat(`/${x}`);
+    // }
+    // if(config.query != {}) url.concat('?');
+    if(config.query != {}){
+        url = url.concat('?');
+        for(let [key, value] of Object.entries(config.query)){
+            url = url.concat(`${key}=${value}&`);
+        }
+        url = url.slice(0, -1);
+    }
+    console.log(url);
+    return url;
+}
+
+
 const useApiCall = () => {
     // config -> obj that specifies the necessity of the API call
     // The hook returns an async function
-    return async (config, body, method='POST', contentType='application/json') => {
-        console.log(config);
-        const res = await fetch(`http://localhost:4573/api/${config.endpoint}`, {
+    return async (config, body = {}, method='POST', contentType='application/json') => {
+        
+        const res = await fetch(createURL(config), {
             method: method,
+            credentials: config.endpoint === 'signup' ? 'omit' : 'include',
             headers:{
                 'Content-Type': contentType
             },
-            body: body
+            body: JSON.stringify(body)
         });
         const data = await res.json();
         return data;
